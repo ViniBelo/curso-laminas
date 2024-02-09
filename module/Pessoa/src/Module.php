07fe2c65fd91233cb32dae005368060f46a09e22
Vinicius Alves Beló <alexguilhermeferreiraa@gmail.com>
2024-02-09 10:47:23 -0300

    Creating Factories

--- module/Pessoa/src/Module.php
+++ module/Pessoa/src/Module.php
@@ -1,12 +1,35 @@
 <?php
 
 namespace Pessoa;
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Db\ResultSet\ResultSet;
+use Laminas\Db\TableGateway\TableGateway;
 use Laminas\ModuleManager\Feature\ConfigProviderInterface;
+use Pessoa\Model\Pessoa;
+use Pessoa\Model\PessoaTable;
 
 class Module implements ConfigProviderInterface
 {
     public function getConfig()
     {
         return include __DIR__."/../config/module.config.php";
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Model\PessoaTable::class => function($container) {
+                    $tableGateway = $container->get(Model\PessoaTableGateway::class);
+                    return new PessoaTable($tableGateway);
+                },
+                Model\PessoaTableGateway::class => function($container) {
+                    $dbAdapter = $container->get(AdapterInterface::class);
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Pessoa());
+                    return new TableGateway('pessoa', $dbAdapter, null, $resultSetPrototype);
+                },
+            ]
+        ];
     }
 }
